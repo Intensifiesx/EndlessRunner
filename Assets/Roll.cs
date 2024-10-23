a930d1a3dcf6595d1636d2365fa804097a2a2c1d
Intensifies <55933220+Intensifiesx@users.noreply.github.com>
2024-10-22 22:39:44 -0700

    Initial commit

---
+++ Assets/Roll.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    // float rotationSpeed = -20f;
+    // //Collision
+    // private void OnCollisionStay2D(Collision2D collision)
+    // {
+    //     // rotate the ball
+    //     if (collision.gameObject.tag == "Floor")
+    //     {
+    //         print("Collided with floor");
+    //         // slowly rotate the ball
+    //         transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    //     }
+    // }
+}
